185621bcae67196d024a6e47a290332728b274fb
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-05-18 20:35:13 +0000

    feat:Learnt Functions

--- lesson-04/build/js/main.js
+++ lesson-04/build/js/main.js
@@ -1,1 +1,57 @@
 "use strict";
+let myName;
+myName = 'John';
+function add(a, b) {
+    return a + b;
+}
+function logMsg(message) {
+    console.log(message);
+}
+let subtract = function (a, b) {
+    return a - b;
+};
+// interface numFunction {
+//     (a:number,b:number) : number
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+//optional parameters:
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+//Default parameters
+const sumAll = (a, b, c = 5) => {
+    return a + b + c;
+};
+//Rest Parameters
+const total = (...nums) => {
+    return nums.reduce((prev, current) => prev + current);
+};
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+//custom type guard
+const isNumber = (value) => {
+    return typeof value === "number" ? true : false;
+};
+const numberOrString = (value) => {
+    if (typeof value === "string") {
+        return "string";
+    }
+    else if (isNumber(value)) {
+        return "number";
+    }
+    return createError("This should never Happen"); //uses of the never type
+};
--- lesson-04/src/main.ts
+++ lesson-04/src/main.ts
@@ -7,3 +7,75 @@ interface Guitarist {
     active?:boolean,
     albums:stringOrNumberArray
 }
+
+
+let myName:"Dave" | "John" | "Amy"
+myName = 'John'
+
+function add(a:number,b:number):number{
+    return a+b
+}
+
+function logMsg(message:any){
+    console.log(message)
+}
+
+let subtract = function(a:number,b:number):number{
+    return a-b
+}
+
+type numFunction = (a:number,b:number) => number
+
+// interface numFunction {
+//     (a:number,b:number) : number
+// }
+
+let multiply:numFunction = function(c,d){
+    return c  * d
+}
+
+//optional parameters:
+const addAll = (a:number, b:number, c?:number):number =>{
+    if(typeof c !== 'undefined'){
+        return a+b+c
+    }
+    return a+b
+}
+
+//Default parameters
+const sumAll = (a:number, b:number, c:number = 5):number =>{
+    return a+b+c
+}
+
+//Rest Parameters
+const total = (...nums:number[]):number =>{
+    return nums.reduce((prev,current)=>prev+current)
+}
+
+const createError = (errMsg:string)=>{
+    throw new Error(errMsg)
+}
+
+const infinite = () =>{
+    let i: number = 1
+    while(true){
+        i++
+        if(i>100) break 
+    }
+}
+
+//custom type guard
+const isNumber = (value:any) : boolean => {
+    return typeof value === "number" ? true : false
+}
+
+
+const numberOrString = (value:number | string):string =>{
+    if(typeof value  === "string"){
+        return "string"
+    }
+    else if(isNumber(value)){
+        return "number"
+    }
+    return  createError("This should never Happen") //uses of the never type
+}
